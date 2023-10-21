3964118aba7c852b90a8a8711fa0dee1f572abbd
alvin-dev <alvinweeb@gmail.com>
2023-10-20 22:46:30 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,93 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
+function onScroll() {
+  colorNavOnScroll()
+  showButtonToTop()
+  fixedMenuOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  //verificar se a seção passou da linha alvo
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  // verificar se a base passou da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+function colorNavOnScroll() {
+  if (scrollY > 0) {
+    navigationn.classList.add('scroll')
+  } else {
+    navigationn.classList.remove('scroll')
+  }
+}
+
+function showButtonToTop() {
+  if (scrollY > 400) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+/*========= Scroll Reveal =========*/
+function fixedMenuOnScroll() {
+  if (scrollY > 75) {
+    navigationn.classList.add('fixed-menu')
+  } else navigationn.classList.remove('fixed-menu')
+}
+
+/*========= Scroll Reveal =========*/
+
+ScrollReveal({
+  reset: true,
+  origin: 'top',
+  distance: '30px',
+  duration: 800
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card,
+  #about,
+  #about header,
+  #about .content
+  `)
